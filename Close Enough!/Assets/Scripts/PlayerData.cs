fe6efd77617aab39a6ecd483128609514d125a39
Rachel Dang <rachel.dang@student.csulb.edu>
2018-03-18 19:10:41 -0700

    Added Menu Navigation scene for UI

---
+++ Close Enough!/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour {
+	public Text text;
+	public string Name;
+
+	public void UpdateText() {
+		Name = text.text;
+	}
+}
