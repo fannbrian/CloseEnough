02c0a280047981296dd36560492a5c0c30008273
jluongh <luong.jenniferh@gmail.com>
2018-04-04 15:46:44 -0700

    Created Timer

---
+++ Close Enough!/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public int timer = 30;
+	public Text countdown;
+	public Text timesUp;
+
+	// Use this for initialization
+	void Start () {
+		timesUp.enabled = false;
+		StartCoroutine ("endTime");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (timer < 0) {
+			StopCoroutine ("endTime");
+			timesUp.enabled = true;
+		} else if (timer < 10) {
+			countdown.text = ":0" + timer;
+		} else {
+			countdown.text = ":" + timer;
+		}
+	}
+
+	IEnumerator endTime() {
+		while (true) {
+			yield return new WaitForSeconds (1);
+			timer--;
+		}
+	}
+}
