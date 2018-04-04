fe07eb43b42047e194f69d905015940c7d00854d
jluongh <luong.jenniferh@gmail.com>
2018-04-04 16:38:20 -0700

    Updated screen capture

--- Close Enough!/Assets/Scripts/ScreenCapture.cs
+++ Close Enough!/Assets/Scripts/ScreenCapture.cs
@@ -1,46 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CloseEnough {
-public class ScreenCapture : MonoBehaviour {
+	public class ScreenCapture : MonoBehaviour {
 
-//	Texture2D screenCap;
+		public Image img;
+		Texture2D texture;
+		bool shot = false;
 
-	public Renderer rend;
+		// Use this for initialization
+		void Start () {
+			texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
 
-	// Use this for initialization
-	void Start () {
-//		screenCap = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
+		}
 
-	}
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				StartCoroutine ("Capture");
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyUp (KeyCode.Space)) {
-			StartCoroutine ("Capture");
-			
+				
+			}
+					
+		}
+
+		void OnGUI() {
+			if (shot) {
+				GUI.DrawTexture (new Rect (10, 10, 600, 450), texture, ScaleMode.StretchToFill);
+
+			}
 		}
 				
-	}
-			
-	IEnumerator Capture() {
-		yield return new WaitForEndOfFrame ();
-		Texture2D texture = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, true);
-		texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0);
-		texture.Apply ();
+		IEnumerator Capture() {
+			yield return new WaitForEndOfFrame ();
 
-		rend.material.mainTexture = texture;
+			texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
+			texture.Apply();
+			shot = true;
 
-
-
-//		screenCap.ReadPixels(new Rect(0, 0, Screen.width, Screen.height),0, 0);
-//		screenCap.Apply ();
-	}
-//
-//	public void Screenshot() {
-//		StartCoroutine ("Capture");
-//	}
+		}
 		
-}
+	}
 }
