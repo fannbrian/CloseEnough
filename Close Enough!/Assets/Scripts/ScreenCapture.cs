e21a638a3aae8637b091d55d427463fb234477f9
jluongh <luong.jenniferh@gmail.com>
2018-04-06 17:26:04 -0700

    Updated Screenshot

--- Close Enough!/Assets/Scripts/ScreenCapture.cs
+++ Close Enough!/Assets/Scripts/ScreenCapture.cs
@@ -1,46 +1,47 @@
-﻿using System.Collections;
+﻿//using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace CloseEnough {
-	public class ScreenCapture : MonoBehaviour {
+public class ScreenCapture : MonoBehaviour {
 
-		public Image img;
-		Texture2D texture;
-		bool shot = false;
+	Texture2D texture;
 
-		// Use this for initialization
-		void Start () {
-			texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+	// Screenshot image for user's drawing 
+	public RawImage img;
+	bool shot = false;
+
+
+	// Use this for initialization
+	void Start () {
+		texture = new Texture2D(Screen.width - 230, Screen.height, TextureFormat.RGB24, false);
+		img.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+			
+	IEnumerator Capture() {
+		yield return new WaitForEndOfFrame ();
+
+		texture.ReadPixels(new Rect(230, 0, Screen.width - 230, Screen.height), 0, 0, false);
+		texture.Apply();
+
+		img.texture = texture;
+
+		shot = true;
+
+	}
+
+	// Screen capture 
+	public void done() {
+		if (!shot) {
+			StartCoroutine ("Capture");
+			img.enabled = true;
 
 		}
-
-		// Update is called once per frame
-		void Update () {
-			if (Input.GetKeyUp (KeyCode.Space)) {
-				StartCoroutine ("Capture");
-
-				
-			}
-					
-		}
-
-		void OnGUI() {
-			if (shot) {
-				GUI.DrawTexture (new Rect (10, 10, 600, 450), texture, ScaleMode.StretchToFill);
-
-			}
-		}
-				
-		IEnumerator Capture() {
-			yield return new WaitForEndOfFrame ();
-
-			texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
-			texture.Apply();
-			shot = true;
-
-		}
-		
 	}
 }
--- Close Enough!/Assets/Scripts/Timer.cs
+++ Close Enough!/Assets/Scripts/Timer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,20 +7,25 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
 	public int timer = 30;
+
 	public Text countdown;
 	public Text timesUp;
+
+	public ScreenCapture screenCap;
 
 	// Use this for initialization
 	void Start () {
 		timesUp.enabled = false;
 		StartCoroutine ("endTime");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (timer < 0) {
 			StopCoroutine ("endTime");
+			screenCap.done ();
+
 			timesUp.enabled = true;
 		} else if (timer < 10) {
 			countdown.text = ":0" + timer;
