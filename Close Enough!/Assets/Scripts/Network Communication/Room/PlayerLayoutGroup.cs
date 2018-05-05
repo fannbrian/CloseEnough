8d898b40d3cd4819e0ad35f0613cc17870dc2446
Brian Fann <fannbrian@gmail.com>
2018-05-04 20:17:47 -0700

    Cleaned up directory again and added a 'Connecting...' page

---
+++ Close Enough!/Assets/Scripts/Network Communication/Room/PlayerLayoutGroup.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+namespace CloseEnough
+{
+    /// <summary>
+    /// Handler for how players are displayed in a room
+    /// <para>
+    /// @Author: Alex Berthon
+    /// @Updated: 5/2/18
+    /// </para>
+    /// </summary>
+    public class PlayerLayoutGroup : PunBehaviour
+    {
+        [SerializeField]
+        private GameObject _playerListingPrefab;
+        private GameObject PlayerListingPrefab
+        {
+            get { return _playerListingPrefab; }
+        }
+
+        private List<PlayerListing> _playerListings = new List<PlayerListing>();
+        private List<PlayerListing> PlayerListings
+        {
+            get { return _playerListings; }
+        }
+        
+        public void InitializeRoom()
+        {
+            while (PlayerListings.Count > 0)
+            {
+                Destroy(PlayerListings[0].gameObject);
+                PlayerListings.RemoveAt(0);
+            }
+            PhotonPlayer[] photonPlayers = PhotonNetwork.playerList;
+            for (int i = 0; i < photonPlayers.Length; i++)
+            {
+                PlayerJoinedRoom(photonPlayers[i]);
+            }
+        }
+
+        //called by photon
+        public override void OnPhotonPlayerConnected(PhotonPlayer photonPlayer)
+        {
+            PlayerJoinedRoom(photonPlayer);
+        }
+
+        //called by photon
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer photonPlayer)
+        {
+            PlayerLeftRoom(photonPlayer);
+        }
+
+
+        private void PlayerJoinedRoom(PhotonPlayer photonPlayer)
+        {
+            if (photonPlayer == null)
+                return;
+
+            PlayerLeftRoom(photonPlayer);
+
+            GameObject playerListingObj = Instantiate(PlayerListingPrefab);
+            playerListingObj.transform.SetParent(transform, false);
+
+            PlayerListing playerListing = playerListingObj.GetComponent<PlayerListing>();
+            playerListing.ApplyPhotonPlayer(photonPlayer);
+
+            PlayerListings.Add(playerListing);
+        }
+
+        private void PlayerLeftRoom(PhotonPlayer photonPlayer)
+        {
+            int index = PlayerListings.FindIndex(x => x.PhotonPlayer == photonPlayer);
+            if (index != -1)
+            {
+                Destroy(PlayerListings[index].gameObject);
+                PlayerListings.RemoveAt(index);
+            }
+        }
+
+        public void OnClickLeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+}
---
+++ Close Enough!/Assets/Scripts/UI/TextDotsEffect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Creates a '...' effect on the text this component is attached to.
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class TextDotsEffect : MonoBehaviour {
+    public int totalDots;
+    public float animationRate;
+
+    int _currentDots;
+    string _initialText;
+    Text _textComponent;
+    float elapsedTime;
+    
+	// Use this for initialization
+	void Start () {
+        _textComponent = GetComponent<Text>();
+        _initialText = _textComponent.text;
+        _currentDots = 0;
+	}
+
+	void Update () {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= 1f / animationRate)
+        {
+            elapsedTime = 0;
+            _currentDots = ++_currentDots % (totalDots+1);
+
+            var resultString = _initialText;
+
+            for(int i = 0; i <= (_currentDots-1); i++)
+            {
+                resultString += '.';
+            }
+
+            _textComponent.text = resultString;
+        }
+	}
+}
